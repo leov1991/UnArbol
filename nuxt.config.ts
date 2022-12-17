622d18c09d1d399f4455f9e6d70a3d8659c2f870
Leonel Villa <leonel.villa.efisat@gmail.com>
2022-12-17 15:28:46 -0300

    Agregar editor Markdown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['simplemde/dist/simplemde.min.css'],
+  plugins: [{ src: '~plugins/simplemde.js', mode: 'client' }],
 })
