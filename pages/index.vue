5310c6df7a30f9fdf3d620be9ff6723859d1549b
Leonel Villa <leonel.villa.efisat@gmail.com>
2023-02-15 15:21:48 -0300

    Mostrar posts en index

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 <template>
     <main>
-      <h2>HOME</h2>
-      <!-- <div v-for="post in posts" :key="post.id">
+      <div v-for="post in posts" :key="post.id">
         <NuxtLink key={post.id} :to="`/posts/${post.id}`">
           <div class="cursor-pointer border-b border-gray-300 pb-4">
             <h2 class="text-xl font-semibold">{{ post.title }}</h2>
-            <p class="text-gray-500 mt-2">Author: {{ post.user_email }}</p>
+            <p class="text-gray-500 mt-2">Autor: {{ post.user_email }}</p>
           </div>
         </NuxtLink>
       </div>
-      <h1 v-if="!posts" class="text-2xl">No posts...</h1> -->
+      <h1 v-if="!posts" class="text-2xl">No posts...</h1>
     </main>
   </template>
   <script setup>
@@ -17,8 +16,8 @@
 
 
 
-  // const supabase = useSupabaseClient();
-  // const {data:posts} = await supabase.from('posts')
-  //       .select('*')
+  const supabase = useSupabaseClient();
+  const {data:posts} = await supabase.from('posts')
+        .select('*')
 
   </script>
